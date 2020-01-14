dc5ec209165cb6a0f2598a46ed10650341bcf551
miteshsevani <msevani@gmail.com>
2020-01-14 12:00:43 +0000

    Updated styling to match designs. Added comments date formating

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,14 +1,19 @@
-const comments = fetch('https://my-json-server.typicode.com/telegraph/frontend-exercise/comments')
+fetch('https://my-json-server.typicode.com/telegraph/frontend-exercise/comments')
   .then(blob => blob.json())
   .then(data => {
     const commentsEl = document.querySelector('.comments')
+    const commentsLength = data.length;
     let commentsHTML = [];
+
+    // Display total number of comments
+    document.querySelector('.comments-length').innerHTML = commentsLength;
+
     data.map(comment => {
       commentsHTML.push(`
         <div class="comment">
           <div class="meta">
             <span class="user">${comment.name}</span>
-            <span class="date">${comment.date}</span>
+            <span class="date">${formatDate(comment.date)}</span>
             <span class="likes">${comment.likes} Likes</span>
           </div>
           <div class="comment-text">
@@ -22,3 +27,18 @@ const comments = fetch('https://my-json-server.typicode.com/telegraph/frontend-e
   .catch(err => {
     throw err
   });
+
+
+  function formatDate(dateToFormat) {
+    const date = new Date(dateToFormat);
+    const day = date.getDate();
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    let hours = date.getHours() - 1; // minus 1 to offset bst time
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    const minutes = date.getMinutes();
+    return `${day} ${month} ${year}, ${hours}:${minutes}${ampm}`
+  }
